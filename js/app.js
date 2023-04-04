34474454480ce204e7254a645c5d42fc3b72276f
Rafael Simas <rafael.simas@al.resilia.com.br>
2023-04-04 17:31:54 -0300

    Adicionei mudanças no layout da tela de login do aluno, refatorei a tela de home e adicionei a funcionalidade do menu toggle

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function abrirMenu(){
+    let menu = document.querySelector('.boxMenuMobile')
+    menu.classList.toggle('telaEscondida')
+}
+
+
+function habilitarModal(){
+    let modal = document.querySelector('.modalSelect')
+    modal.classList.remove('telaEscondida')
+}
